6c3ac93538526a5505a6322dc893d3e19ed0623a
Michael <mihui.net@outlook.com>
2015-06-05 01:35:52 +0800

    Adding native methods

--- speech-android-wrapper/src/main/java/com/ibm/cio/opus/OggOpus.java
+++ speech-android-wrapper/src/main/java/com/ibm/cio/opus/OggOpus.java
@@ -4,9 +4,12 @@ import com.ibm.cio.util.Logger;
 
 public class OggOpus {
     private static final String TAG = OggOpus.class.getSimpleName().toString();
-
-    public static native int decode( String s, String o, int srate );
-
+    public static native void initAudio();
+    public static native void startRecorder( int sample_rate);
+    public static native void stopRecorder();
+    public static native int encode( String s, int sample_rate );
+    public static native int decode( String s, String o, int sample_rate );
+    public static native float volume();
     static {
         System.loadLibrary("oggopus");
         Logger.e(TAG, "OggOpus library is loaded...");
