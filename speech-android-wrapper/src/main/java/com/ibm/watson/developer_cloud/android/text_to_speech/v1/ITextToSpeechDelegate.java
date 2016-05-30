cd5512a7f0f08f0134c4e11f11b410f24c974577
Michael <mihui@cn.ibm.com>
2016-05-31 02:24:53 +0800

    Added back TTS delegate

---
+++ speech-android-wrapper/src/main/java/com/ibm/watson/developer_cloud/android/text_to_speech/v1/ITextToSpeechDelegate.java
@@ -0,0 +1,27 @@
+package com.ibm.watson.developer_cloud.android.text_to_speech.v1;
+
+/**
+ * Created by mihui on 5/29/16.
+ */
+public interface ITextToSpeechDelegate {
+    /**
+     * This method will be called when TTS request start
+     */
+    public void onTTSStart();
+
+    /**
+     * This method will be called when the TTS audio data is back, but before audio plays
+     */
+    public void onTTSWillPlay();
+
+    /**
+     * This method will be called when the TTS request is finished and audio play stops
+     */
+    public void onTTSStopped();
+
+    /**
+     * This mehtod will be called when there is an error of requesting TTS
+     * @param statusCode
+     */
+    public void onTTSError(int statusCode);
+}
