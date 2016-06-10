172fc6d2074b6bba47a95874a2f9a158d8b84e81
Michael <mihui@cn.ibm.com>
2016-06-10 18:24:54 +0800

    Separated configurations and make an abstrat AuthConfiguration class align with iOS version

---
+++ speech-android-wrapper/src/main/java/com/ibm/watson/developer_cloud/android/speech_common/v1/AuthConfiguration.java
@@ -0,0 +1,13 @@
+package com.ibm.watson.developer_cloud.android.speech_common.v1;
+
+import java.net.URI;
+
+/**
+ * Created by mihui on 6/5/16.
+ */
+public class AuthConfiguration {
+    public String basicAuthUsername;
+    public String basicAuthPassword;
+    public URI apiEndpoint;
+    public String apiURL;
+}
