dd65627719b16f7a350945a976bb8173ccb00151
Michael <mihui@cn.ibm.com>
2016-07-20 01:21:57 +0800

    Handle byte array directly intead of using short

--- speech-android-wrapper/src/main/java/com/ibm/watson/developer_cloud/android/speech_to_text/v1/audio/AudioCaptureRunnable.java
+++ speech-android-wrapper/src/main/java/com/ibm/watson/developer_cloud/android/speech_to_text/v1/audio/AudioCaptureRunnable.java
@@ -18,8 +18,9 @@ package com.ibm.watson.developer_cloud.android.speech_to_text.v1.audio;
 
 import java.lang.Thread;
 import android.util.Log;
+
+import android.media.AudioFormat;
 import android.media.AudioRecord;
-import android.media.*;
 import android.media.MediaRecorder.AudioSource;
 
 import java.nio.ByteBuffer;
@@ -33,7 +34,7 @@ import java.nio.ByteOrder;
 public class AudioCaptureRunnable implements Runnable {
 
     private static final String TAG = "AudioCaptureThread";
-    private boolean mStop = false;
+    private boolean isRecording = false;
     private boolean mStopped = false;
     private int mSamplingRate = -1;
     private IAudioConsumer mIAudioConsumer = null;
@@ -42,8 +43,8 @@ public class AudioCaptureRunnable implements Runnable {
     // THREAD_PRIORITY_URGENT_AUDIO = "Standard priority of the most important audio threads"
     public AudioCaptureRunnable(int iSamplingRate, IAudioConsumer IAudioConsumer) {
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-        mSamplingRate = iSamplingRate;
-        mIAudioConsumer = IAudioConsumer;
+        this.mSamplingRate = iSamplingRate;
+        this.mIAudioConsumer = IAudioConsumer;
     }
 
     // once the thread is started it runs nonstop until it is stopped from the outside
@@ -52,34 +53,34 @@ public class AudioCaptureRunnable implements Runnable {
         AudioRecord recorder = null;
 
         try {
-            int iN = Math.max(mSamplingRate/2,AudioRecord.getMinBufferSize(mSamplingRate,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT));
-            short[] buffer = new short[iN]; // ASR latency depends on the length of this buffer, a short buffer is good for latency
-            // because the ASR will process the speech sooner, however it will introduce some network overhead because each packet comes
-            // with a fixed amount of protocol-data), also I have noticed that some servers cannot handle too many small packages
+            int bufferSize = Math.max(this.mSamplingRate / 2, AudioRecord.getMinBufferSize(this.mSamplingRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT));
+            byte[] buffer = new byte[bufferSize];
 
-            // initialize the recorder (buffer size will be at least 1/4th of a second)
-            recorder = new AudioRecord(AudioSource.MIC, mSamplingRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, iN);
+            recorder = new AudioRecord(AudioSource.MIC, mSamplingRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
             recorder.startRecording();
-            Log.d(TAG, "recording started!");
-            while(!mStop) {
+            this.isRecording = true;
 
-                int r = recorder.read(buffer,0,buffer.length);
-                long v = 0;
-                for (int i = 0; i < r; i++) {
-                    v += buffer[i] * buffer[i];
+            Log.d(TAG, "Recording started");
+            while(isRecording) {
+                int byteCount = recorder.read(buffer, 0, buffer.length);
+                if(byteCount > 0) {
+                    long v = 0;
+                    for (int i = 0; i < byteCount; i++) {
+                        v += buffer[i] * buffer[i];
+                    }
+                    double amplitude = v / (double) byteCount;
+                    double volume = 0;
+                    if (amplitude > 0) volume = 10 * Math.log10(amplitude);
+
+                    mIAudioConsumer.onAmplitude(amplitude, volume);
+
+                    // convert to an array of bytes and send it to the server
+                    byte[] bytes = ByteBuffer.allocate(byteCount).order(ByteOrder.LITTLE_ENDIAN).put(buffer, 0, byteCount).array();
+                    mIAudioConsumer.consume(bytes);
                 }
-                double amplitude = v / (double) r;
-                double volume = 0;
-                if(amplitude > 0)
-                    volume = 10 * Math.log10(amplitude);
-                mIAudioConsumer.onAmplitude(amplitude, volume);
-
-                // convert to an array of bytes and send it to the server
-                ByteBuffer bufferBytes = ByteBuffer.allocate(r*2);
-                bufferBytes.order(ByteOrder.LITTLE_ENDIAN);
-                bufferBytes.asShortBuffer().put(buffer,0,r);
-                byte[] bytes = bufferBytes.array();
-                mIAudioConsumer.consume(bytes);
+                else {
+                    Log.e(TAG, "--------------> 0 short count~");
+                }
             }
         }
         catch(Throwable x) {
@@ -103,7 +104,7 @@ public class AudioCaptureRunnable implements Runnable {
 
     // this function is intended to be called from outside the thread in order to stop the thread
     public void end() {
-        mStop = true;
+        this.isRecording = false;
         // waiting loop, it waits until the thread actually finishes
         while(!mStopped) {
             try {
